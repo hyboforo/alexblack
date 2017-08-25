42db882ab1878e936a4ed3c7ba178e4613c8d383
hyboforo <hyboforo@gmail.com>
2017-08-25 23:50:52 +0000

    edited Product.rb class

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ mount_uploader :image3, ImageUploader
 validates :title, :description,:image1, :image2, :image3, presence: true
 validates :price, numericality:{greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
-validates :image_url, allow_blank: true, format: {
+validates :image1, :image2, :image3, allow_blank: true, format: {
 with: %r{\.(gif|jpg|png)\Z}i,
 message: 'must be a URL for GIF, JPG or PNG image.'
 }
