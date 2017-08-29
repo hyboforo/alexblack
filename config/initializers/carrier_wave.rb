418c1d341151d1f9c7f50dc6c5e20fd2a18972b4
hyboforo <hyboforo@gmail.com>
2017-08-29 23:22:30 +0000

    configured AWS s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -22,7 +22,7 @@
     :aws_secret_access_key  => 'VyIdwkKhcrJtjk2CjW9UqB7NZ5p18b57XF1mLgLP',     # required
     :region                 => 'US East (Ohio)'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'alexblack'               # required
+  config.fog_directory  = 'http://s3.amazonaws.com/alexblack'               # required
   config.asset_host       = 'http://s3.amazonaws.com'           # optional, defaults to nil
   config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
